afa5b90eaeb8236b5235d9e100fcc4dc7041ac35
Hideki1202 <matheus.ueno@germinare.org.br>
2026-02-01 02:51:57 -0300

    feat: registration screen

--- programacao-assincrona-frontend/src/app/app-routing-module.ts
+++ programacao-assincrona-frontend/src/app/app-routing-module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Login } from './pages/login/login';
+import { Matricula } from './pages/matricula/matricula';
 
 const routes: Routes = [
   { path: 'login', component: Login },
+  { path: 'matricula', component: Matricula }
 ];
 
 @NgModule({
---
+++ programacao-assincrona-frontend/src/app/pages/matricula/matricula.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-matricula',
+  standalone: false,
+  templateUrl: './matricula.html',
+  styleUrl: './matricula.css',
+})
+export class Matricula {
+
+}
