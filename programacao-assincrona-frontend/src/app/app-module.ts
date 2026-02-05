f8384588274df9400785ad869168c5d909b01402
Hideki1202 <matheus.ueno@germinare.org.br>
2026-02-05 08:54:42 -0300

    páginas de estudantes e meu perfil

--- programacao-assincrona-frontend/src/app/app-module.ts
+++ programacao-assincrona-frontend/src/app/app-module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Login } from './pages/login/login';
 import { Matricula } from './pages/matricula/matricula';
+import { MyProfile } from './pages/my-profile/my-profile';
+import { Table } from './components/table/table';
+import { Students } from './pages/students/students';
 
 @NgModule({
   declarations: [
     App,
     Login,
-    Matricula
+    Matricula,
+    MyProfile,
+    Table,
+    Students
   ],
   imports: [
     BrowserModule,
